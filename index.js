ea5d141019d6730015cca53cb93f301b7c0c85e2
MinDz204 <160019862+MinDz204@users.noreply.github.com>
2024-06-02 12:09:21 +0700

    index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ client.once('ready', () => {
   setTimeout(() => {
     console.log('\x1b[32m%s\x1b[0m', `|    🎯 Activity sucessfully set!`);
     client.user.setPresence({
-      activities: [{ name: `Type $help for Info`, type: ActivityType.Custom }],
+      activities: [{ name: `YuongzMin On Top👑 Type y!help `, type: ActivityType.Custom }],
       status: 'dnd',
     });
   }, 2000); 
